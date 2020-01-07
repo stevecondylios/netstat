2535341834fa0558fbb08b78a03a18958d22e267
Steve Condylios <steve.condylios@gmail.com>
2020-01-07 18:26:16 +1100

    Passes checks --as-cran

--- R/netstat-package.R
+++ R/netstat-package.R
@@ -2,7 +2,7 @@
 #' @docType package
 #'
 #' @details It has the goal of providing a convenient way of retrieving and parsing commonly used
-#'     TCP port information provided via the netstat command line utility.
+#'    TCP port information provided via the netstat command line utility.
 #'
 #'
 #' @author Steve Condylios \email{steve.condylios@gmail.com}
